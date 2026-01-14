ac4a8428ca8441bc06c0148b4188aeeaed16db86
Bard <readsbard@gmail.com>
2026-01-14 21:49:41 +0300

    init

---
+++ src/main/java/com/ozan/mirgos/MirgosApplication.java
@@ -0,0 +1,13 @@
+package com.ozan.mirgos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MirgosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MirgosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ozan/mirgos/MirgosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozan.mirgos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MirgosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
