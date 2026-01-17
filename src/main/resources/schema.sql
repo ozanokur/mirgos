071d3d07ff2463a19c0fae993db2590b9c04202e
Bard <readsbard@gmail.com>
2026-01-17 13:26:37 +0300

    missing indexes

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,12 @@
 CREATE INDEX IF NOT EXISTS idx_stores_location
 ON stores
 USING GIST (location);
+
+CREATE INDEX IF NOT EXISTS idx_courier_locations_courier_time
+ON courier_locations (courier_id, time);
+
+CREATE UNIQUE INDEX IF NOT EXISTS idx_courier_distances_courier_id
+ON courier_distances (courier_id);
+
+CREATE INDEX IF NOT EXISTS idx_entrances_courier_store_event_time
+ON entrances (courier_id, store_id, event_time);
